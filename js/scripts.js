c6665485b12c7eacf4205f1d69b084bfbf80bad7
Katarzyna Skotnicka <ka.skotnicka@gmail.com>
2018-02-10 14:29:22 +0100

    Update js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $('#search').click(searchCountries);
 
 function searchCountries() {
     var countryName = $('#country-name').val();
-if(!countryName.length) countryName = 'Poland';
+if(!countryName.length) countryName = '.....';
 $.ajax({
         url: url + countryName,
         method: 'GET',
